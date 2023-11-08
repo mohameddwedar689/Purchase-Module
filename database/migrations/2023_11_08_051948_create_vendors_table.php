26e355c6c0e0e4cd42199e00d64b5f11ca0bacfd
mohameddwedar689 <mohameddwedar689@gmail.com>
2023-11-08 07:43:23 +0200

    [create] model and make migration (done)

---
+++ database/migrations/2023_11_08_051948_create_vendors_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vendors', function (Blueprint $table) {
+            $table->id();
+            $table->string('company');
+            $table->string('fname');
+            $table->string('lname');
+            $table->string('email');
+            $table->integer('phone');
+            $table->string('address');
+            $table->string('type');
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vendors');
+    }
+};
