68ae392f50c78f3318b6f05a944b5cd626903944
mohameddwedar689 <mohameddwedar689@gmail.com>
2023-11-10 15:23:46 +0200

    [update] routes on web page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Auth;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,8 +12,11 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+//Route::view('/products1','products');
+Route::resource('product', ProductController::class);
+Auth::routes();
 
-// Route::resource('product', ProductController::class);
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
 
